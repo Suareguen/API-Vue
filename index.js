bfee8f517abbca8a149846034bcc337bbe18b2e2
Suarenguen <adrian.suarez.monzon@gmail.com>
2023-12-29 20:38:36 +0000

    r

--- index.js
+++ index.js
@@ -20,6 +20,32 @@ const mongooseStart = async () => {
 const initializeApp = () => {
   const app = express()
     .use(express.json())
+    .post('/webhook', (req, res) => {
+      console.log('Webhook recibido:', req.body);
+    
+      // Captura la carga útil del webhook
+      const payload = req.body;
+    
+      // Asegúrate de que es un evento de pull request
+      if (payload.pull_request) {
+        console.log(`Evento de Pull Request: ${payload.action}`);
+    
+        switch (payload.action) {
+          case 'opened':
+            console.log('Pull Request abierto:', payload.pull_request);
+            // Aquí puedes hacer algo con la información del pull request abierto
+            break;
+          case 'closed':
+            console.log('Pull Request cerrado:', payload.pull_request);
+            // Manejar un pull request cerrado
+            break;
+          // Incluye otros casos según lo necesites
+        }
+      }
+    
+      // Responde a GitHub que recibiste el evento
+      res.status(200).send('Evento de Webhook recibido');
+    })
     .use(cors())
     .use(morgan("dev"))
     .use('/api', require('./api/router/index'))
