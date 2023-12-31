cbea9cdc09c2a707ba12e92bbdec40f56ac78392
Suarenguen <adrian.suarez.monzon@gmail.com>
2023-12-31 16:33:42 +0000

    test

--- api/utils/githubWebhookFunctions/updatePullRequests.js
+++ api/utils/githubWebhookFunctions/updatePullRequests.js
@@ -5,7 +5,9 @@ const Lab = require("../../models/labs.model")
 const updatePullRequests = async (event, body) => {
     try {
         console.log(event)
-        console.log(body)
+        console.log(body.sender.login)
+        console.log(body.repository.name)
+        console.log(body.owner.name)
     } catch (error) {
         throw new Error(error)
     }    
